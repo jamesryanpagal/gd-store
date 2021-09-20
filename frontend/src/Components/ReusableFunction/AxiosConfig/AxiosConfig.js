c624167295e723fb1d5c681d162988fd6d21887c
jamesryanpagal <ryanpagal02@gmail.com>
2021-09-21 01:28:45 +0800

    deployment

--- frontend/src/Components/ReusableFunction/AxiosConfig/AxiosConfig.js
+++ frontend/src/Components/ReusableFunction/AxiosConfig/AxiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosConfig = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://gd-store-mern.herokuapp.com/",
 });
 
 export default axiosConfig;
